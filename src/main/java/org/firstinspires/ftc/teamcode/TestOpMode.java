4fc8201f7fdc2e8bc7cbf746010b5fcf54be41bb
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2024-09-21 21:40:34 -0600

    First working version

---
+++ src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.overture.ftc.OverDcMotor;
+
+@Autonomous(name = "Auto", group = "Test")
+public class TestOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        OverDcMotor dcMotor = new OverDcMotor(hardwareMap, "lower_arm");
+
+        waitForStart();
+
+        System.out.println("Waiting 6 seconds...");
+        dcMotor.setPower(-0.25);
+        Thread.sleep(6000);
+        dcMotor.setPower(0.25);
+        System.out.println("Done");
+    }
+}
